e70b6fe8f4aadc2e47093d90f9ec9650312f823e
MizuRyu <02rlmdev@gmail.com>
2025-05-09 12:26:48 +0900

    Initial Commit

---
+++ Night-Core-Player/Night_Core_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Night_Core_PlayerApp.swift
+//  Night-Core-Player
+//
+//  Created by RyuichiroMizutani on 2025/05/09.
+//
+
+import SwiftUI
+
+@main
+struct Night_Core_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
